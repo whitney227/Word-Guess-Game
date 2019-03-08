185d00520ff4bd685e2bec895b06d645b49da502
Whitney Crawford <whitneylcrawford@gmail.com>
2019-03-08 15:39:32 -0500

    added globalvariables to js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+//Global Variables//
+//=========================================================
+    //Array for mystery word
+    var wordOptions=["washington","lincoln", "roosevelt", "adams", "nixon", "kennedy", "johnson", "bush", "clinton", "obama", "carter",];
+    var selectedWord="";
+    var lettersInWord=[];
+    var numBlanks=0;
+    var blanksAndSuccesses=[];
+    var wrongLetters=[];
+//Functions//
+
+
+//Main Process//
