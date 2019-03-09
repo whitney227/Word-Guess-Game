9685425c13d1dbd647233b781b8102b488c4a64a
Whitney Crawford <whitneylcrawford@gmail.com>
2019-03-09 00:11:03 -0500

    finished functions and added style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,18 +11,18 @@ var guessesLeft=9;
 
 //Functions//
 //==============================================================================================================================================
-function startGame (){
+function startGame(){
     selectedWord=wordOptions[Math.floor(Math.random()*wordOptions.length)];
-    console.log(selectedWord);
-
     lettersInWord = selectedWord.split("");
-    console.log(lettersInWord);
-
     numBlanks = lettersInWord.length;
-    console.log(numBlanks);
 
+    console.log(selectedWord);
+    console.log(lettersInWord);
+    console.log(numBlanks);
+    
     //Reset for next round
     guessesLeft=9;
+    wrongletters=[];
     blanksAndSuccesses=[];
 
     //Blanks generated for each round
@@ -33,8 +33,64 @@ function startGame (){
 
     //HTML changes to refelct each round
     document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById("guessesRemaining").innerHTML = guessesLeft;
+    document.getElementById("winCounter").innerHTML = winCount;
+}
+function checkLetter(letter){
+    var isLetterInWord = false;
+
+    //Check if letter is in mystery word
+    for (var i=0; i<numBlanks; i++){
+        if (selectedWord [i] == letter) {
+            isLetterInWord = true;
+        }
+    }
+    //Check where the letter is located in mystery work
+    if(isLetterInWord){
+        for (var i=0; i<numBlanks; i++){
+            if (selectedWord[i]==letter){
+                blanksAndSuccesses[i]=letter;
+            }
+        }
+    }
+    //Letter is not found
+    else {
+        wrongLetters.push(letter);
+        guessesLeft--   
+    }
+
+    console.log(blanksAndSuccesses);
+}
+
+function roundComplete(){
+    //update HTML panels
+    document.getElementById("guessesRemaining").innerHTML = guessesLeft;
+    document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById("lettersGuessed").innerHTML = wrongLetters.join(" ");
+
+    if(lettersInWord.toString()==blanksAndSuccesses.toString()){
+        winCount++;
+        alert("You Won!");
+        
+    //update HTMl and start new game    
+    document.getElementById("winCounter").innerHTML=winCount;
+    startGame();
+    }
+
+    else if (guessesLeft == 0) {
+        alert("You are out of guesses. Try again!");
+    document.getElementById("guessesRemaining").innerHTML=guessesLeft;
+    }   
 }
 
 //Main Process//
 //===============================================================================================================================================
 startGame();
+//Register keyclicks
+document.onkeyup = function(event) {
+    var letterPicked = String.fromCharCode(event.keyCode).toLowerCase();
+    checkLetter(letterPicked);  
+    roundComplete();   
+
+    console.log(letterPicked);
+}
